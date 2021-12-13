742c5a9f93c73a39c598eff2b8992086e44bb782
Jaime Rogstad <jaimerogstad@gmail.com>
2021-12-12 20:52:43 -0500

    Add table relationships

---
+++ tables-alter-relationships.sql
@@ -0,0 +1,24 @@
+ALTER TABLE participant
+ADD FOREIGN KEY(client)
+REFERENCES client(client_id)
+ON DELETE SET NULL;
+
+ALTER TABLE course
+ADD FOREIGN KEY(teacher)
+REFERENCES teacher(teacher_id)
+ON DELETE SET NULL;
+
+ALTER TABLE course
+ADD FOREIGN KEY(client)
+REFERENCES client(client_id)
+ON DELETE SET NULL;
+
+*This is a many to many - connecting participant and course - A course may have many participants and a participant may take many courses*
+
+CREATE TABLE takes_course (
+  participant_id INT,
+  course_id INT,
+  PRIMARY KEY(participant_id, course_id),
+  FOREIGN KEY(participant_id) REFERENCES participant(participant_id) ON DELETE CASCADE,
+  FOREIGN KEY(course_id) REFERENCES course(course_id) ON DELETE CASCADE
+);
