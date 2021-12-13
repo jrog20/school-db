f6043f8013b6edd42acf312469c632cacdae513b
Jaime Rogstad <jaimerogstad@gmail.com>
2021-12-12 20:46:32 -0500

    Add tables

---
+++ tables-creation.sql
@@ -0,0 +1,37 @@
+CREATE TABLE teacher (
+  teacher_id INT PRIMARY KEY,
+  first_name VARCHAR(40) NOT NULL,
+  last_name VARCHAR(40) NOT NULL,
+  language_1 VARCHAR(3) NOT NULL,
+  language_2 VARCHAR(3),
+  dob DATE,
+  tax_id INT UNIQUE,
+  phone_no VARCHAR(20)
+  );
+
+CREATE TABLE client (
+  client_id INT PRIMARY KEY,
+  client_name VARCHAR(40) NOT NULL,
+  address VARCHAR(60) NOT NULL,
+  industry VARCHAR(20)
+);
+
+CREATE TABLE participant (
+  participant_id INT PRIMARY KEY,
+  first_name VARCHAR(40) NOT NULL,
+  last_name VARCHAR(40) NOT NULL,
+  phone_no VARCHAR(20),
+  client INT
+);
+
+CREATE TABLE course (
+  course_id INT PRIMARY KEY,
+  course_name VARCHAR(40) NOT NULL,
+  language VARCHAR(3) NOT NULL,
+  level VARCHAR(2),
+  course_length_weeks INT,
+  start_date DATE,
+  in_school BOOLEAN,
+  teacher INT,
+  client INT
+);
